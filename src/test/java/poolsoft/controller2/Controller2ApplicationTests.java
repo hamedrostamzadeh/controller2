09bad0328de7f0da8a9abeb4c8518c2457396d7e
Hamed Rostamzadeh <hamed.rostamzadeh@voxcarrier.com>
2022-02-23 02:33:54 +0330

    start c1

---
+++ src/test/java/poolsoft/controller2/Controller2ApplicationTests.java
@@ -0,0 +1,13 @@
+package poolsoft.controller2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Controller2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
