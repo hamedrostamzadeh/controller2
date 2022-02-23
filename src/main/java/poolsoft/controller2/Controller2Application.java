75610e02819101b335f02691a244e3adcb9d507d
Hamed Rostamzadeh <hamed.rostamzadeh@voxcarrier.com>
2022-02-23 04:04:08 +0330

    update to m1

--- src/main/java/poolsoft/controller2/Controller2Application.java
+++ src/main/java/poolsoft/controller2/Controller2Application.java
@@ -2,13 +2,13 @@ package poolsoft.controller2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import poolsoft.model.M;
+import poolsoft.model.M1;
 
 @SpringBootApplication
 public class Controller2Application {
 
     public static void main(String[] args) {
-        M m = new M();
+        M1 m = new M1();
         SpringApplication.run(Controller2Application.class, args);
     }
 
