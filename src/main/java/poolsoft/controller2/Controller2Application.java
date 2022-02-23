732b185b218bb51d6a0c855b4e87913e20a2b4e7
Hamed Rostamzadeh <hamed.rostamzadeh@voxcarrier.com>
2022-02-23 03:56:14 +0330

    c2 works fine.

--- src/main/java/poolsoft/controller2/Controller2Application.java
+++ src/main/java/poolsoft/controller2/Controller2Application.java
@@ -2,11 +2,13 @@ package poolsoft.controller2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import poolsoft.model.M;
 
 @SpringBootApplication
 public class Controller2Application {
 
     public static void main(String[] args) {
+        M m = new M();
         SpringApplication.run(Controller2Application.class, args);
     }
 
